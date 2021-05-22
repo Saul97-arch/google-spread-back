a8e3aa496827c2e4ca225374ac481e634271896a
Saulo <duart.herrera@gmail.com>
2021-05-22 17:01:56 -0300

    Params in post req send with sucess time to implement the logic!

--- routes/updateByParam.js
+++ routes/updateByParam.js
@@ -4,6 +4,8 @@ const { google } = require("googleapis");
 const { credentials } = require("../services/spreadsheet");
 
 router.post("/", async (req, res) => {
+  console.log('rota update')
+  const { val1, val2, cellVall } = req.query;
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -17,42 +19,31 @@ router.post("/", async (req, res) => {
   const spreadsheetId = "1fO850ZLI6L-1yKVlAaM783VC9usIKg673c4mBuECGWo";
 
   try {
-    /*  await sheets.spreadsheets.values.update({
-      auth,
-      spreadsheetId: spreadsheetId,
-      range: "F3:F3",
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [["34"]],
-      },
-    }).data; */
     const request = {
       auth,
       spreadsheetId: spreadsheetId,
       includeValuesInResponse: true,
       resource: {
-        valueInputOption: "USER_ENTERED", 
+        valueInputOption: "USER_ENTERED",
         data: [
           {
             dataFilter: {
               gridRange: {
-                startColumnIndex: 4,
-                startRowIndex: 0,
+                startColumnIndex: val1,
+                startRowIndex: val2,
                 sheetId: 0,
               },
             },
-            values: [["CAXETAAAAA"]],
+            values: [[cellVall]],
           },
         ],
       },
-
     };
-
-    await sheets.spreadsheets.values.batchUpdateByDataFilter(request).data;
+    const result = await sheets.spreadsheets.values.batchUpdateByDataFilter(request).data;
   } catch (err) {
     console.error(err);
   }
-
+  console.log(val1, val2);
   res.send("RONALDO");
 });
 
